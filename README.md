e8a678e137f67fdfb11b514a2e557a47376827c8
Luke Bogdanovic <19232691@studentmail.ul.ie>
2022-11-07 15:11:05 +0000

    Adding ASP.NET files

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # CS4125-System-Analysis-and-Design
+
+## How To Write Code for this Project
+
+Writing code should be done with the UniLibrary folder as the root within vscode.
+
+Before running the Program.cs file this command should be run in your terminal:`dotnet dev-certs https --trust`
+**Note!!! This command will only work on Windows. If using Linux see your specific Linux distribution's documentation for trusting a certificate.**
---
+++ UniLibrary/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using UniLibrary.Models;
+using System.Text.Encodings.Web;
+
+namespace UniLibrary.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // localhost/HelloWorld/
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+        // localhost/HelloWorld/Welcome/3?name=Seanie
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+    }
+}
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
