f77af35e44e78abeb1d3a9610ebf945db06353c5
ChopsLambe <58043682+ChopsLambe@users.noreply.github.com>
2022-11-29 13:26:37 +0000

    temp state

--- UniLibrary/Models/Computers.cs
+++ UniLibrary/Models/Computers.cs
@@ -10,7 +10,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace UniLibrary.Models
 {
-    public class Computer
+    public abstract class Computer
     {
         public int ID { get; set; }
         [StringLength(60, MinimumLength = 3)]
@@ -22,7 +22,53 @@ namespace UniLibrary.Models
         [StringLength(30)]
         [Required]
         public bool Availability { get; set; }   
+
+        //constructer 
+        public Computer(int ID, string? PCNum, string? OS)
+        {
+            this.ID = ID;
+            this.PCNum = PCNum;
+            this.OS = OS;
+            this.Availability = true;
+        }
     }
+
+    public class PC : Computer
+    {
+        //constructer
+        public PC (int ID, string? PCNum, string? OS)
+        : base(ID, PCNum, OS)
+        {
+        }
+    }
+
+    // public abstract class ComputerObserver
+    // {
+    //     public abstract void Update();
+    // }
+
+    // public class ConcreteObserver : ComputerObserver
+    // {
+    //     private string name;
+    //     private string observerState;
+    //     private PC subject;
+
+    //     // Constructor
+
+    //     public ConcreteObserver(
+    //         PC pc, string name)
+    //     {
+    //         this.subject = subject;
+    //         this.name = name;
+    //     }
+
+    //     public override void Update()
+    //     {
+    //        // observerState = subject.SubjectState;
+    //         Console.WriteLine("Observer {0}'s new state is {1}",
+    //             name, observerState);
+    //     }
+    // }
 }
 
 
