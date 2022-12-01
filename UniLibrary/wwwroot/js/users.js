f018ae01b01a830ca265cae3f908d3471ae52714
ChopsLambe <58043682+ChopsLambe@users.noreply.github.com>
2022-12-01 11:41:50 +0000

    merge conflicts fixes?

---
+++ UniLibrary/wwwroot/js/users.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+  $("#users").on("click", ".js-delete", function () {
+    let button = $(this);
+    Swal.fire({
+      title: `Are you sure you want to delete this user ?`,
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#d33",
+      cancelButtonColor: "#5ea2bc",
+      confirmButtonText: "Confirm",
+    })
+      .then((result) => {
+        if (result.isConfirmed) {
+          $.ajax({
+            url: "/users/delete/" + button[0].dataset["userID"],
+            type: "DELETE",
+            success: function (data) {
+              if (data) {
+                if (data.success) {
+                  window.location.reload();
+                  toastr.success(data.message);
+                } else {
+                  toastr.error(data.message);
+                }
+              }
+            },
+          });
+        }
+      })
+      .catch(() => {
+        toastr.error("An unexpected error occurred please try again later !");
+      });
+  });
+});
