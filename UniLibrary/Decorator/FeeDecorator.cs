32553e27d079767754424bb3fa4129133151f62e
Luke Bogdanovic <19232691@studentmail.ul.ie>
2022-12-01 04:43:00 +0000

    CS4125-25 Added func to LoanController

--- UniLibrary/Decorator/FeeDecorator.cs
+++ UniLibrary/Decorator/FeeDecorator.cs
@@ -1,15 +1,16 @@
 namespace UniLibrary.Decorator
 {
-    public class FeeDecorator :BaseFee
+    public class FeeDecorator : BaseFee
     {
-        BaseFee BaseFee;
+        BaseFee? BaseFee;
         public void AttachTo(BaseFee Fee)
         {
             this.BaseFee = Fee;
+            this.Fee = Fee.Fee;
         }
         public override void CreateFee()
         {
-            BaseFee.CreateFee();
+            BaseFee!.CreateFee();
         }
     }
 }
