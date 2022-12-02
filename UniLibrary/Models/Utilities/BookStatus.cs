715e5f987a0721864ff792f735e206e0d331b8c7
ChopsLambe <58043682+ChopsLambe@users.noreply.github.com>
2022-12-02 14:26:44 +0000

    implementing strategy design pattern

--- UniLibrary/Models/Utilities/BookStatus.cs
+++ UniLibrary/Models/Utilities/BookStatus.cs
@@ -1,7 +1,41 @@
+using UniLibrary.Interfaces;
+
 namespace UniLibrary.Models.Utilities
 {
+
     public static class BookStatus
     {
         public const int Max = 9;
     }
+
+    public class BookStatusAdmin : IStrategy
+    {
+
+        public int setBookStatus(int bookStatus)
+        {
+            return bookStatus * 3;
+        }
+
+    }
+
+    public class BookStatusUnderGrad : IStrategy
+    {
+
+        public int setBookStatus(int bookStatus)
+        {
+            return bookStatus;
+        }
+
+    }
+
+    public class BookStatusPostGrad : IStrategy
+    {
+
+        public int setBookStatus(int bookStatus)
+        {
+            return bookStatus * 2;
+        }
+
+    }
+
 }
